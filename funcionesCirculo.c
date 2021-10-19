8ce88a30ec186de6f4b74452e909ef5f7605c93b
Miguel Salazar <miguelsalazar88@gmail.com>
2021-10-18 23:09:50 -0500

    Se agregó el programa que calcula los datos de un círculo

---
+++ funcionesCirculo.c
@@ -0,0 +1,52 @@
+// Autores: Diego Bermúdez y Miguel Salazar
+// Versión 1
+// Este programa encuentra el diámetro, circunferencia y área de un círculo a partir
+// del valor del radio que ingresa el usuario.
+
+#include <stdio.h>
+
+//Declaración de la función:
+
+double diametro(double radio);
+double circunferencia(double radio);
+double area(double radio);
+
+//Método main.
+int main(){
+
+    //Declaración de variables.
+
+    double radio;
+
+    //Se le pide al usuario que ingrese el valor del radio.
+
+    printf("**** BIENVENIDO AL PROGRAMA QUE CALCULA DIAMETRO, CIRCUNFERENCIA Y AREA DE UN CIRCULO **** \n");
+
+    printf("Por favor introduzca el valor del radio del círculo en metros: ");
+    scanf("%lf", &radio);
+    printf("\n");
+
+    //Se ejecuta la lógica.
+
+    printf("Resultados: \n");
+    printf("Diámetro       = %f metros \n", diametro(radio));
+    printf("Circumferencia = %f metros\n", circunferencia(radio));
+    printf("Area           = %f metros cuadrados\n",area(radio));
+    printf("\n");
+
+
+    return 0;
+}
+
+//Definición de la función:
+
+double diametro(double radio){
+    return (radio * 2);
+}
+double circunferencia(double radio){
+    return(3.1416 * radio);
+}
+double area(double radio){
+    return(3.1416 * (radio * radio));
+}
+
