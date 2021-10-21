8ccc9ab84e45d07b95a32f9dc22bbb0d5accfd47
Miguel Salazar <miguelsalazar88@gmail.com>
2021-10-21 17:28:23 -0500

    Se agregó linearSearch.c

--- linearSearch.c
+++ linearSearch.c
@@ -13,10 +13,20 @@ int main(){
 
     //Se declaran las variables.
 
-    int arreglo[] = {1,29,32,45,65,38,51,90,23,64,86,48,92,46,89,44,33,55,76,10,1,3,5,7,9};
+    int arreglo[] = {15,29,32,45,65,38,51,90,23,64,86,48,92,46,89,44,33,55,76,10,1,3,5,7,9};
 
+    printf("**** Bienvenido al programa que implementa un algoritmo de búsqueda lineal **** \n");
+    printf("En algún lugar del arreglo se encuentra el número 65, y se buscará mediante este algoritmo. \n");
 
+    printf("El arreglo es: \n");
 
+    for(int i=0; i<25; i++){
+        printf("%d  ",arreglo[i]);
+    }
+    printf("\n");
+    
+    printf("Se implementa el algoritmo... \n");
+    busquedaLineal(65,25,arreglo);
     return 0;
 }
 
@@ -26,10 +36,10 @@ void busquedaLineal(int n, int tamanio, int arreglo[n]){
     int i;
     bool encontro = false;
     
-    for(i = 0; i < n; i++){
+    for(i = 0; i < tamanio; i++){
         if(arreglo[i] == n){
             encontro = true;
-            printf("el numero %d se encuentra en la posición %d", n, i);
+            printf("el numero %d se encuentra en la posición %d \n", n, i);
         }
     }
 
